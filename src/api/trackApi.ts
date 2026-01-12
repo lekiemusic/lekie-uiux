5a370fff6ca247ec34f7631e0e5c929846c13a87
yeen322 <yemonmon321@gmail.com>
2026-01-12 18:11:00 +0900

    #4 feat(domain.user): 메인 페이지 supabase api 연동 - 하드 코딩되어 있는 목업 데이터 삭제 -> supabase에 저장된 데이터를 api로 불러와서 연동 - supabase, tanstack query 도입을 통해 데이터 페칭 및 상태 관리 개선
    Resolves: #4
    See also: None

---
+++ src/api/trackApi.ts
@@ -0,0 +1,21 @@
+import { createClient } from "@supabase/supabase-js";
+import type {Track} from "../types/ApiDataTypes.ts";
+
+const supabase = createClient(
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
+);
+
+// api 호출
+const fetchTracks = async (): Promise<Track[]> => {
+	const { data, error } = await supabase
+		.from("jamendo_tracks")
+		.select("id, title, artist_name, audio_url, image_url, duration");
+
+	if (error) throw error;
+
+	return data;
+};
+
+export default fetchTracks;
+
--- src/types/PropsTypes.ts
+++ src/types/PropsTypes.ts
@@ -4,12 +4,5 @@
 export interface PlayerProps {
 	selectedPanel: number;
 	onPanelChange: (tab: number) => void;
-	currentTrackId: string;
 }
 
-/**
- * PlayerControl Props 인터페이스
- */
-export interface PlayerControlProps {
-	currentTrackId: string;
-}
